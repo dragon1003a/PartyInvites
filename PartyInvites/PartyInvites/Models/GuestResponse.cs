94f89832c0b567e8ade219d2c4ca7d9475718970
Admin <dragon1003a@wp.pl>
2018-10-18 11:55:37 +0200

    wszystko_bez_cssa

---
+++ PartyInvites/PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage ="Proszę podać imię")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="Proszę podać adres e-mail")]
+        [RegularExpression(".+\\@.+\\..+" , ErrorMessage ="Proszę podać prawidłowy adres mailowy")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="Proszę podać numer telefonu")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać czy weźmiesz udział w wydarzeniu")]
+        public bool? WillAttend { get; set; }
+    }
+}
